3b7ddfac9063b9a1710ba84c07abca80c136f8e9
Irsan Arisandy <irsanarisandy@hotmail.com>
2018-12-14 14:48:47 +1300

    Initialize.

---
+++ README.md
@@ -0,0 +1,9 @@
+# Simply run:
+```
+go build && ./golang-quiz
+```
+
+# Flags:
+--csv = name of csv file (default is problems.csv)
+
+--limit = time limit in seconds (default is 30 seconds)
---
+++ main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"bufio"
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+)
+
+func main() {
+	csvFilename := flag.String("csv", "problems.csv", "A CSV file in the format of question,answer.")
+	timeLimit := flag.Int("limit", 30, "The time limit for the quiz in seconds.")
+	flag.Parse()
+
+	file, err := os.Open(*csvFilename)
+
+	if err != nil {
+		exit(fmt.Sprintf("Failed to open the CSV file: %s .", *csvFilename))
+	}
+
+	r := csv.NewReader(file)
+
+	lines, err := r.ReadAll()
+
+	if err != nil {
+		exit("Failed to parse the provided CSV file.")
+	}
+
+	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
+	problems := parseLines(lines)
+	reader := bufio.NewReader(os.Stdin)
+	correct := 0
+
+problemloop:
+	for i, p := range problems {
+		fmt.Printf("Problem #%d: %s = ", i+1, p.q)
+		answerCh := make(chan string)
+		go func() {
+			answer, _ := reader.ReadString('\n')
+			answer = strings.TrimSpace(answer)
+			answerCh <- answer
+		}()
+		select {
+		case <-timer.C:
+			fmt.Printf("You scored %d out of %d.\n", correct, len(problems))
+			break problemloop
+		case answer := <-answerCh:
+			if answer == p.a {
+				fmt.Println("Correct")
+				correct++
+			} else {
+				fmt.Println("Incorrect")
+			}
+		}
+	}
+
+	fmt.Printf("You scored %d out of %d.\n", correct, len(problems))
+}
+
+func parseLines(lines [][]string) []problem {
+	ret := make([]problem, len(lines))
+	for i, line := range lines {
+		ret[i] = problem{
+			q: line[0],
+			a: strings.TrimSpace(line[1]),
+		}
+	}
+	return ret
+}
+
+type problem struct {
+	q string
+	a string
+}
+
+func exit(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
+}
